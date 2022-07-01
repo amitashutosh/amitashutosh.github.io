e87dd20b06f825534ab47d0d11595cc3a76ad7c1
Amit <amashuto@cisco.com>
2022-07-01 02:30:01 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Amit Ashutosh's GitHub Page
+          Amit Ashutosh's GitHub
         </p>
       </header>
     </div>
